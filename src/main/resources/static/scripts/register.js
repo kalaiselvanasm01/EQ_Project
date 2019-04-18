cabc98d550b6e4d87d9676944b408e47d31942f9
subhashini Mohanraj <rangan2050@gmail.com>
2019-04-18 20:01:25 +0530

    validation added in register EQ

--- src/main/resources/static/scripts/register.js
+++ src/main/resources/static/scripts/register.js
@@ -2,16 +2,179 @@ $(document).ready(function () {
     $("#registerScreen").modal({backdrop:"static"});
     $("#registerBtn").click(function (event) {
         event.preventDefault();
-        var password = $("#registerForm #password").val().trim();
+        var password = $("#registerForm #opassword").val().trim();
         var cPassword = $("#registerForm #cPassword").val().trim();
         if(password!=='' && cPassword!=='') {
             if (password === cPassword) {
                 $("#registerForm #originalPassword").val(password);
-                $(this).closest("form#registerForm").submit();
+                  
             } else {
                 alert("Password Mismatch!!!");
             }
         }
+        
+        if($("#firstName").val() != ""){
+         if($("#dateOfBirth").val() != ""){
+        	 if($("#fatherName").val()  != ""){
+        		 if($("#address").val()  != ""){
+        			 if($("#religion").val()  != ""){
+        				 if($("#expObtHSCMark").val()  != ""){
+        					 if($("#expectedDept").val()  != ""){
+        						 if($("#opassword").val()  != ""){
+        						 if($("#email").val()  != ""){
+        							 if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(registerForm.email.value)){
+        								 if($("#gender").val() != ""){
+        									 if($("#username").val() != ""){
+        										 if($("#nationality").val() != ""){
+        											 if($("#cPassword").val() != ""){
+        												 if($("#bloodGroup").val() != ""){
+        												 
+        												 $(this).closest("form#registerForm").submit();
+        												 }else{
+        													 $("#bloodGroup").focus();
+        												 }
+        											 }else{
+        												 $("#cPassword").focus();
+        											 }
+        											 
+        										 }else{
+        											 $("#nationality").focus();
+        										 }
+        										 
+        									 }else{
+        										 $("#username").focus();
+        									 }
+        									 
+        								 }else{
+        									 $("#gender").focus();
+        								 }
+        								
+        							 }
+        							 else{
+        								 alert("enter the valid mail id");
+        								 $("#email").focus();
+        							 }
+        						    
+        						 } else{
+        							    $("#email").focus();
+        							 }
+        							 
+        						 }else{
+        							 $("#opassword").focus();
+        						 }
+        						 
+        					 }else{
+        						 $("#expectedDept").focus();  
+        					 }
+        					 
+        				 }else{
+        					 $("#expObtHSCMark").focus(); 
+        				 }
+        				 
+        			 }else{
+        				 $("#religion").focus();
+        			 }
+        	 
+        		 }else{
+        			 $("#address").focus();
+        			 }
+        		 }
+        		 else{
+        		 $("#fatherName").focus();
+        	 }
+         }	else{
+        	 $("#dateOfBirth").focus();
+         }
+        }else{
+        	$("#firstName").focus();
+        }
+	        
+//		
+// if($("#firstName").val() == ""){
+// $("#firstName").focus();
+//	
+//
+// if($("#dateOfBirth").val() == ""){
+// $("#dateOfBirth").focus();
+// return false;
+// }
+// if($("#fatherName").val() == ""){
+// $("#fatherName").focus();
+// return false;
+// }
+// if($("#address").val() == ""){
+// $("#address").focus();
+// return false;
+// }
+// if($("#religion").val() == ""){
+// $("#religion").focus();
+// return false;
+// }
+// if($("#expObtHSCMark").val() == ""){
+// $("#expObtHSCMark").focus();
+// return false;
+// }
+// if($("#expectedDept").val() == ""){
+// $("#expectedDept").focus();
+// return false;
+// }
+// if($("#opassword").val() == ""){
+// $("#opassword").focus();
+// return false;
+// }
+//
+// if($("#email").val() == ""){
+// $("#email").focus();
+// event.preventDefault();
+// return false;
+// }
+// if
+// (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(registerForm.email.value))
+// {
+// return (true)
+// }
+// else{
+// alert("You have entered an invalid email address!");
+// $("#email").focus();
+// return false;
+// }
+//	
+// if($("#gender").val() == ""){
+// $("#gender").focus();
+// return false;
+// }
+// if($("#username").val() == ""){
+// $("#username").focus();
+// return false;
+// }
+// if($("#nationality").val() == ""){
+// $("#nationality").focus();
+// return false;
+// }
+// if($("#hscGroup").val() == ""){
+// $("#hscGroup").focus();
+// return false;
+// }
+//
+//
+//
+//
+// if($("#bloodGroup").val() == ""){
+// $("#bloodGroup").focus();
+// return false;
+// }
+//
+// if($("#cPassword").val() == ""){
+// console.log("cpswd",$("#cPassword").val());
+// $("#cPassword").focus();
+// return false;
+// }
+// if($("#phNumber").val() == ""){
+// $("#phNumber").focus();
+// return false;
+// }
+
+
     });
     $("#userLoginLink").on('click', function () {
         $("#loginScreen").modal({backdrop: "static"});
@@ -38,4 +201,21 @@ $(document).ready(function () {
             });
         }
     });
+    
+//	
+// if($("#email").val() != ""){
+//		 
+// if
+// (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(registerForm.email.value))
+// {
+// $(this).closest("form#registerForm").submit();
+// return true;
+// }
+// else{
+// alert("You have entered an invalid email address!");
+// $("#email").focus();
+// return false;
+// }
+//	 
+// }
 });
